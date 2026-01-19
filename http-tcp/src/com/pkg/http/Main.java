d0cfb87d7774f09689184c2a830cd26fbf2c1368
Sayan <mainmandal23@gmail.com>
2026-01-19 09:52:00 +0530

    adding socket and fixing the issues

--- http-tcp/src/com/pkg/http/Main.java
+++ http-tcp/src/com/pkg/http/Main.java
@@ -1,109 +1,172 @@
 package com.pkg.http;
 
-import java.io.*;
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.concurrent.*;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingDeque;
 
 public class Main 
 {
-	public static BlockingQueue<String> getLinesChannels(InputStream fis)
+	public static void main(String[] args)
 	{
-		BlockingQueue<String> queB = new LinkedBlockingQueue<String>(10);
-		
-		new Thread(()->{
-			
-			String currentLine = "";
-			try
+		ServerSocket conn = null; 
+		FileInputStream fir =null; 
+		try 
+		{
+			conn = new ServerSocket(42069);
+			System.out.println("server is running on 42069 port number");
+
+			//put the output 
+			while(true)
+			{
+					Socket cliSocket = conn.accept();
+					System.out.println("Connection is established");
+					BlockingQueue<String> ans = getLinesChannel(cliSocket.getInputStream());
+					try {
+						while(true)
+						{
+							String line;
+							line = ans.take();
+							if(line.equals("EOF"))
+							{
+								System.out.println("conneciton  is closed");
+								break;
+							}
+							
+							System.out.println(line);
+						}
+					} 
+					catch (InterruptedException e)
+					{	
+						Thread.currentThread().interrupt();
+					}
+					finally
+					{
+						if(fir!=null)
+						{
+							try 
+							{
+								fir.close();
+							} 
+							catch (IOException e)
+							{
+								e.printStackTrace();
+							}
+						}
+						if(cliSocket!=null)
+						{
+							try 
+							{
+								cliSocket.close();
+							} 
+							catch (IOException e)
+							{
+							}
+						}
+					}
+				}
+
+		} 
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	//create the moduler function 
+	public static BlockingQueue<String> getLinesChannel(InputStream fir)
+	{
+		BlockingQueue<String> bq = new LinkedBlockingDeque<>();
+
+		// Start the thread
+		Thread worker =new Thread(()->{
+			ByteArrayOutputStream byteArr =null;
+			try 
 			{
 				byte[] buffer = new byte[8];
+				byteArr = new ByteArrayOutputStream();
 				int byteRead;
-				
-				while ((byteRead = fis.read(buffer)) != -1)
+				while((byteRead=fir.read(buffer))!=-1)
 				{
-					String chunk = new String(buffer, 0, byteRead);
-//                System.out.println(chunk);
-					String [] parts = chunk.split("\n",-1);
-					for(int i =0; i<parts.length-1;++i)
+					for(int i = 0;i< byteRead; ++i)
 					{
-						 queB.put(currentLine+parts[i]);
-						currentLine="";
+						byte b = buffer[i];
+						if(b == 10)
+						{
+							String fullLine = byteArr.toString("UTF-8");
+							try 
+							{
+								bq.put(fullLine);
+							}
+							catch (InterruptedException e)
+							{
+								Thread.currentThread().interrupt();
+							}
+							byteArr.reset();
+						}
+						else
+						{
+							byteArr.write(b);
+						}
 					}
-					currentLine +=parts[parts.length-1];
 				}
+//				old code it's have an error
+//				if(byteArr.size()>0)
+//				{
+//					bq.put("EOF");
+//				}
 				
-				if(!currentLine.isEmpty())
+				// FIXED CODE
+
+				// 1. First, rescue any leftover text that didn't have a \n
+				if(byteArr.size() > 0)
 				{
-					queB.put(currentLine);
+				    String lastLine = byteArr.toString("UTF-8");
+				    try
+				    {
+						bq.put(lastLine);
+					}
+				    catch (InterruptedException e)
+				    {
+						Thread.currentThread().interrupt();
+					}
 				}
-				queB.put("EOF");
+
+				// 2. ALWAYS send the Poison Pill, no matter what happened before
+				try
+				{
+					bq.put("EOF");
+				} 
+				catch (InterruptedException e) 
+				{
+					Thread.currentThread().interrupt();
+				}
 			} 
-			
-			catch (IOException | InterruptedException e) 
+			catch (IOException e)
 			{
 				e.printStackTrace();
 			}
-			
-			finally
+			finally 
 			{
-				if (fis != null)
+				if(fir!=null)
 				{
-					try
+					try 
 					{
-						fis.close();
-					}
+						fir.close();
+					} 
 					catch (IOException e)
 					{
 						e.printStackTrace();
 					}
 				}
 			}
-		}).start();
-		return queB;
-	}
-    public static void main(String[] args) 
-    {
-    	ServerSocket socket = null;
-        try 
-        {
-        	socket  = new ServerSocket(6969);
-        	System.out.println("Server listening on port 6969...");
-        	while(true)
-        	{	
-        		Socket cliSocket = socket.accept();
-        		System.out.println("Accepted connection");
-        		BlockingQueue<String> queue = getLinesChannels(cliSocket.getInputStream());
-        		while(true)
-        		{
-        			String msg = queue.take();
-        			
-        			if(msg.equals("EOF"))
-        			{
-        				System.out.println("Connection closed");
-        				break;
-        			}
-        			System.out.println(msg);
-        		}
-        	}
+			
+		});
 
-		} 
-        catch (IOException | InterruptedException e)
-        {
-			e.printStackTrace();
-		}
-        finally
-        {
-        	if(socket!=null)
-        	{
-        		try
-        		{
-        			socket.close();
-        		}
-        		catch(IOException e)
-        		{
-        			e.printStackTrace();
-        		}
-        	}
-        }
-    }
+		worker.start();
+		return bq;
+	}
 }
